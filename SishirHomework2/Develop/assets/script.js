62a87829c6d2cd9a36ee1bffe6931af08d2381fb
Sishir <sishir.bhattarai@outlook.com>
2021-01-13 20:14:51 -0700

    added files

---
+++ SishirHomework2/Develop/assets/script.js
@@ -0,0 +1,15 @@
+var name = ["Sishir" , "Kam" , "Ram" , "Peter" ];
+
+for (var i = 0; i < name.length ; i++) {
+console.log ( "I am great" + name);
+
+}
+
+  // Creating an array of vegetables.
+  var vegetables = ["Carrots", "Peas", "Lettuce", "Tomatoes"];
+
+  // Looping through each item in the array and logging a message to the console.
+  for (var i = 0; i < vegetables.length; i++) {
+    console.log("I love " + vegetables[i]);
+  }
+
